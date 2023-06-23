0dec141958011491a07d0a3225a54082cad49744
Headbwoi <headbwoi25@gmail.com>
2023-06-23 17:37:17 +0100

    Add_Change_Password_Ui

--- src/app/(app)/settings/security/Security.tsx
+++ src/app/(app)/settings/security/Security.tsx
@@ -1,11 +1,56 @@
 "use client"
 
-import { Button } from "@/client/components/Buttons"
+import { Button, SubmitButton } from "@/client/components/Buttons"
+import { Eye, EyeSlash, PasswordIcon } from "@/client/components/Icons"
 import { Toggle } from "@/client/components/UiElements"
+import { passwordRegex } from "@/constants/regex"
+import { zodResolver } from "@hookform/resolvers/zod"
+import { Modal } from "@mantine/core"
 import { useState } from "react"
+import { useForm } from "react-hook-form"
+import { toast } from "react-hot-toast"
+import { z } from "zod"
+
+type ChangePasswordType = {
+  currentPassword: string
+  newPassword: string
+}
+
+const ChangePasswordSchema = z.object({
+  currentPassword: z
+    .string()
+    .min(2, { message: "Please enter your old password" }),
+  newPassword: z
+    .string()
+    .min(8, { message: "Please enter a strong new password" })
+    .regex(passwordRegex),
+})
 
 function Security() {
   const [rememberMe, setRememberMe] = useState(false)
+  const [showChangePasswordModal, setShowChangePasswordModal] = useState(false)
+  const [passwordType, setPasswordType] = useState<"password" | "text">(
+    "password"
+  )
+
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm<ChangePasswordType>({
+    resolver: zodResolver(ChangePasswordSchema),
+  })
+
+  const handlePasswordToggle = () => {
+    passwordType == "password"
+      ? setPasswordType("text")
+      : setPasswordType("password")
+  }
+
+  const onsubmit = (values: ChangePasswordType) => {
+    console.log(values)
+  }
+
   return (
     <>
       <section className="max-w-lg mx-auto space-y-8">
@@ -15,8 +60,73 @@ function Security() {
           <Toggle defaultValue={rememberMe} setDefaultValue={setRememberMe} />
         </div>
 
-        <Button content="Change Password" variant="empty" />
+        <Button
+          content="Change Password"
+          variant="empty"
+          onClick={() => setShowChangePasswordModal(true)}
+        />
       </section>
+
+      {showChangePasswordModal && (
+        <Modal
+          onClose={() => setShowChangePasswordModal(false)}
+          opened={showChangePasswordModal}
+          title="Change Password"
+          centered
+        >
+          <form onSubmit={handleSubmit(onsubmit)} className="space-y-4">
+            {/* old password */}
+            <div className="w-full space-y-2">
+              <label htmlFor="current-password">Current Password</label>
+              <div className="w-full relative group">
+                <input
+                  type={passwordType}
+                  id="current-password"
+                  {...register("currentPassword")}
+                  className=" w-full flex items-center h-14 bg-input_bg_light dark:bg-input_bg_dark text-primary_dark dark:text-primary font-semibold pl-12 pr-5 focus:ring-1 focus-visible:ring-pri_btn outline-none rounded-lg"
+                  placeholder="current password"
+                />
+                <PasswordIcon />
+                <div
+                  className="absolute top-1/2 -translate-y-1/2 right-14"
+                  onClick={handlePasswordToggle}
+                >
+                  {passwordType == "password" ? <EyeSlash /> : <Eye />}
+                </div>
+              </div>
+              <p className="text-sm text-red-500">
+                {errors.currentPassword?.message}
+              </p>
+            </div>
+
+            {/* new password */}
+            <div className="w-full space-y-2">
+              <label htmlFor="new-password">New Password</label>
+              <div className="w-full relative group">
+                <input
+                  type={passwordType}
+                  id="new-password"
+                  {...register("newPassword")}
+                  className=" w-full flex items-center h-14 bg-input_bg_light dark:bg-input_bg_dark text-primary_dark dark:text-primary font-semibold pl-12 pr-5 focus:ring-1 focus:ring-pri_btn outline-none rounded-lg"
+                  placeholder="new password"
+                />
+                <PasswordIcon />
+                <div
+                  className="absolute top-1/2 -translate-y-1/2 right-14"
+                  onClick={handlePasswordToggle}
+                >
+                  {passwordType == "password" ? <EyeSlash /> : <Eye />}
+                </div>
+              </div>
+              <p className="text-sm text-red-500">
+                {errors.newPassword?.message}
+              </p>
+            </div>
+
+            <SubmitButton content="Submit" variant="filled" />
+          </form>
+        </Modal>
+      )}
     </>
   )
 }
---
+++ src/client/components/Icons/index.ts
@@ -0,0 +1,8 @@
+export { default as Eye } from "./Eye"
+export { default as EyeSlash } from "./EyeSlash"
+export { default as PasswordIcon } from "./PasswordIcon"
+export { default as ChevronRightIcon } from "./ChevronRight"
+export { default as EditIcon } from "./EditIcon"
+export { default as FilterIcon } from "./FilterIcon"
+export { default as GoogleIcon } from "./Googleicon"
+export { default as SuccessIcon } from "./SuccessIcon"
