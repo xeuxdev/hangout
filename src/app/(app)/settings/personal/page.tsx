670fc3516a06ae1f06be64f5151543673185c59d
Headbwoi <headbwoi25@gmail.com>
2023-06-23 17:10:14 +0100

    add suspense to personal route

--- src/app/(app)/settings/personal/page.tsx
+++ src/app/(app)/settings/personal/page.tsx
@@ -1,6 +1,7 @@
 import NavHeader from "@/client/components/Navigation/NavHeader"
 import { serverSession } from "@/lib/auth/serverSession"
 import { UserProfile } from "@/types"
+import { Suspense } from "react"
 
 export const metadata = {
   title: "Personal Information",
@@ -24,24 +25,26 @@ async function page() {
       <NavHeader content="Personal Information" />
 
       <section className="mx-auto max-w-lg space-y-6 pb-14">
-        <div className="w-full bg-input_bg_light dark:bg-input_bg_dark p-6 rounded-xl">
-          {data.name}
-        </div>
-        <div className="w-full bg-input_bg_light dark:bg-input_bg_dark p-6 rounded-xl">
-          {data.userName}
-        </div>
-        <div className="w-full bg-input_bg_light dark:bg-input_bg_dark p-6 rounded-xl">
-          {data.email}
-        </div>
-        <div className="w-full bg-input_bg_light dark:bg-input_bg_dark p-6 rounded-xl">
-          {data.birthday}
-        </div>
-        <div className="w-full bg-input_bg_light dark:bg-input_bg_dark p-6 rounded-xl">
-          {data.gender}
-        </div>
-        <div className="w-full bg-input_bg_light dark:bg-input_bg_dark p-6 rounded-xl">
-          {data.phone}
-        </div>
+        <Suspense>
+          <div className="w-full bg-input_bg_light dark:bg-input_bg_dark p-6 rounded-xl">
+            {data.name}
+          </div>
+          <div className="w-full bg-input_bg_light dark:bg-input_bg_dark p-6 rounded-xl">
+            {data.userName}
+          </div>
+          <div className="w-full bg-input_bg_light dark:bg-input_bg_dark p-6 rounded-xl">
+            {data.email}
+          </div>
+          <div className="w-full bg-input_bg_light dark:bg-input_bg_dark p-6 rounded-xl">
+            {data.birthday}
+          </div>
+          <div className="w-full bg-input_bg_light dark:bg-input_bg_dark p-6 rounded-xl">
+            {data.gender}
+          </div>
+          <div className="w-full bg-input_bg_light dark:bg-input_bg_dark p-6 rounded-xl">
+            {data.phone}
+          </div>
+        </Suspense>
       </section>
     </>
   )
