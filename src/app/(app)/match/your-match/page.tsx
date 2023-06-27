53d4aa811b833627a4107107568fea3b3058e9ba
Headbwoi <headbwoi25@gmail.com>
2023-06-27 14:42:14 +0100

    add your match page

--- src/app/(app)/match/your-match/page.tsx
+++ src/app/(app)/match/your-match/page.tsx
@@ -1,7 +1,38 @@
+import { SearchIcon } from "@/client/components/Icons"
+import NavHeader from "@/client/components/Navigation/NavHeader"
+import { NewMatchCard } from "@/features/match"
+import { serverSession } from "@/lib/auth/serverSession"
+import { UserData } from "@/types"
+import axios from "axios"
 import React from "react"
 
-function YourMatchPage() {
-  return <div>YourMatchPage</div>
+async function YourMatchPage() {
+  const session = await serverSession()
+
+  const users = (await axios(`${process.env.FRONTEND_URL}/api/users/all`).then(
+    (res) => {
+      return res.data
+    }
+  )) as UserData[]
+
+  const filteredUsers = users.filter((user) => user._id !== session?.user.id)
+
+  return (
+    <>
+      <div className="flex items-center justify-between">
+        <NavHeader content="New Match" />
+        <div className="-mt-10">
+          <SearchIcon />
+        </div>
+      </div>
+
+      <section className="grid grid-cols-2 lg:grid-cols-3 gap-3 ">
+        {filteredUsers.map((user) => (
+          <NewMatchCard user={user} key={user._id} />
+        ))}
+      </section>
+    </>
+  )
 }
 
 export default YourMatchPage
