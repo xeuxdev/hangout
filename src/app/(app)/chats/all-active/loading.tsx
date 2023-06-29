ee1fc0116b7eb808472cdcfd7436a53543a0abd7
Headbwoi <headbwoi25@gmail.com>
2023-06-29 14:46:23 +0100

    add loading for all active page

---
+++ src/app/(app)/chats/all-active/loading.tsx
@@ -0,0 +1,35 @@
+import { SearchIcon } from "@/client/components/Icons"
+import NavHeader from "@/client/components/Navigation/NavHeader"
+import React from "react"
+
+function AllActiveLoader() {
+  return (
+    <>
+      <NavHeader content="All Active" />
+
+      <section className="mt-10 space-y-3 pb-20">
+        {Array(20)
+          .fill(0)
+          .map((_, idx) => (
+            <div
+              className="flex items-center justify-between h-[3.75rem] w-full"
+              key={idx}
+            >
+              {/*  */}
+              <div className="flex items-center gap-4 w-full">
+                <div className="w-[3.75rem] h-[3.75rem] rounded-full bg-gray-400 dark:bg-gray-600 animate-pulse" />
+
+                <div className="space-y-1">
+                  <div className="w-56 h-7 rounded-md bg-gray-400 dark:bg-gray-600 animate-pulse" />
+
+                  <div className="w-20 h-4 rounded-md bg-gray-400 dark:bg-gray-600 animate-pulse" />
+                </div>
+              </div>
+            </div>
+          ))}
+      </section>
+    </>
+  )
+}
+
+export default AllActiveLoader
--- src/app/(app)/chats/all-active/page.tsx
+++ src/app/(app)/chats/all-active/page.tsx
@@ -1,7 +1,55 @@
+import NavHeader from "@/client/components/Navigation/NavHeader"
+import Text from "@/client/components/Typography/Text"
+import Image from "next/image"
 import React from "react"
 
-function AllActivePage() {
-  return <div>AllActivePage</div>
+export const metadata = {
+  title: "All Active Chats",
+  description: "See All your Active Matches",
+}
+
+async function AllActivePage() {
+  const users = (await fetch("https://randomuser.me/api/?results=20").then(
+    (res) => {
+      return res.json()
+    }
+  )) as { results: any[] }
+
+  return (
+    <>
+      <NavHeader content="All Active" />
+
+      <section className="mt-10 space-y-3 pb-20">
+        {users.results.map((user, idx) => (
+          <div
+            className="flex items-center justify-between"
+            key={user.name.first + user.name.last}
+          >
+            {/*  */}
+            <div className="flex items-center gap-4">
+              <Image
+                src={user.picture.thumbnail}
+                alt={user.name.first}
+                width={60}
+                height={60}
+                className="rounded-full"
+              />
+
+              <div className="space-y-1">
+                <Text
+                  content={`${user.name.first}  ${user.name.last}`}
+                  size="lg"
+                  font="semibold"
+                />
+
+                <Text content="hey there" size="xs" font="normal" />
+              </div>
+            </div>
+          </div>
+        ))}
+      </section>
+    </>
+  )
 }
 
 export default AllActivePage
